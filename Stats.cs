4b15ea001ac58f8724ec9d53dea0d6b2253415d9
Dean Michael Tang <deanext3@gmail.com>
2021-12-21 02:35:50 +1100

    initial commit

---
+++ Stats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Fenwick_Technical_Task
+{
+    class Stats
+    {
+        static void Main(string[] args)
+        {
+            // To check the length of 
+            // Command line arguments  
+            if(args.Length > 0)
+            {
+                Console.WriteLine("Arguments Passed by the Programmer:");  
+              
+                // To print the command line 
+                // arguments using foreach loop
+                foreach(Object obj in args)  
+                {  
+                    Console.WriteLine(obj);       
+                }  
+            }  
+              
+            else
+            {
+                Console.WriteLine("No command line arguments found.");
+            }
+        }
+    }
+}
