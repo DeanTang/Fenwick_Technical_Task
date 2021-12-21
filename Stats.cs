423ac0879d5706b56b6dabbc285ffa7b41c11a05
Dean Michael Tang <deanext3@gmail.com>
2021-12-21 11:04:07 +1100

    Added comments and handled file not found

--- Stats.cs
+++ Stats.cs
@@ -5,27 +5,33 @@ namespace Fenwick_Technical_Task
 {
     class Stats
     {
+        /* Verifies if value is a decimal number */
         static bool IsDecimal(String value)
         {
             decimal number = 0;
             return decimal.TryParse(value, out number);
         }
+
         static void Main(string[] args)
         {
             // To check the length of 
             // Command line arguments   
             if(args.Length > 0)
             {
+                /* --- Start of Record function --- */
+                // If first argument is record
                if (args[0] == "record")
                {
                    // The record function needs at least 3 arguments to be correct
                    if (args.Length >= 3)
                    {
+                       // Get file path
                        string path = @args[1];
 
                         // Create text file if the file doesn't exist
                         if (!File.Exists(path))
                         {
+                            /* From argument 3 onwards are the values to be recorded */
                             for (int i = 2; i < args.Length; i++)
                             {
                                     if (IsDecimal(args[i]))
@@ -38,6 +44,7 @@ namespace Fenwick_Technical_Task
                                     }
                                     else
                                     {
+                                        // Error handling: Not a decimal number
                                         Console.WriteLine("Invalid argument: {0} - This command requires a decimal number after the file name.",args[i]);
                                     }
                             }
@@ -45,6 +52,7 @@ namespace Fenwick_Technical_Task
                         }
                         else
                         {
+                            // If file exists
                             for (int i = 2; i < args.Length; i++)
                             {
                                 if (IsDecimal(args[i]))
@@ -64,82 +72,114 @@ namespace Fenwick_Technical_Task
                    }
                    else
                    {
+                       // The record function needs at least 3 arguments to be correct, 2 more excluding the 'record' argument
                        Console.WriteLine("Invalid input: Record needs at least 2 other arguments: filename and value.");
                    }
                }
+               /* --- End of Record function --- */
                else
                {
+                   /* --- Start of Summary function --- */
                    if (args[0] == "summary")
                    {
+                       // Get file name
                        string path = @args[1];
-                       using (StreamReader sr = File.OpenText(path))
+
+                       // Check if file exists
+                       if (!File.Exists(path))
                        {
-                            string s = "";
-                            decimal total = 0;
-                            decimal count = 0;
-
-                            // Assuming that the numbers would not go beyond these extremes
-                            decimal min = 9999999999;
-                            decimal max = -9999999999;
-
-                            while ((s = sr.ReadLine()) != null)
+                           // File not found
+                           Console.WriteLine("Error: File does not exist: " + path);
+                       }
+                       else
+                       {
+                           using (StreamReader sr = File.OpenText(path))
                             {
-                                if (IsDecimal(s))
-                                {
-                                    decimal temp = Convert.ToDecimal(s);
-                                    total += temp;
-                                    count++;
+                                // Initialize tracker variables
+                                    string s = "";
+                                    decimal total = 0;
+                                    decimal count = 0;
 
-                                    if (temp < min) 
+                                    // Assuming that the numbers would not go beyond these extremes
+                                    decimal min = 9999999999;
+                                    decimal max = -9999999999;
+
+                                    // Traverse the text file line by line
+                                    while ((s = sr.ReadLine()) != null)
                                     {
-                                        min = temp;
+                                        // Checks if the line can be converted to a decimal value
+                                        if (IsDecimal(s))
+                                        {
+                                            decimal temp = Convert.ToDecimal(s);
+                                            total += temp;
+                                            count++;
+
+                                            // Check for new minimum
+                                            if (temp < min) 
+                                            {
+                                                min = temp;
+                                            }
+
+                                            // Check for new maximum
+                                            if (temp > max)
+                                            {
+                                                max = temp;
+                                            }
+                                        }
                                     }
 
-                                    if (temp > max)
+                                    /* Display in a nice table */
+
+                                    string[] table_outputs = new string[5];
+                                    table_outputs[0] = "| # of Entries    | " + count;
+                                    table_outputs[1] = "| Min. value      | " + min;
+                                    table_outputs[2] = "| Max. value      | " + max;
+                                    table_outputs[3] = "| Total value     | " + total;
+                                    table_outputs[4] = "| Avg. value      | " + total/count;
+
+                                    int max_length = 0;
+
+                                    // Determines the maximum length of all strings
+                                    for (int i = 0; i < table_outputs.Length; i++)
                                     {
-                                        max = temp;
+                                        if (table_outputs[i].Length > max_length)
+                                        {
+                                            max_length = table_outputs[i].Length;
+                                        }
                                     }
-                                }
-                            }
 
-                            string[] table_outputs = new string[5];
-                            table_outputs[0] = "| # of Entries    | " + count;
-                            table_outputs[1] = "| Min. value      | " + min;
-                            table_outputs[2] = "| Max. value      | " + max;
-                            table_outputs[3] = "| Total value     | " + total;
-                            table_outputs[4] = "| Avg. value      | " + total/count;
+                                    // Make a top line string
+                                    // +1 for the extra '|'
+                                    Console.WriteLine(new string('-', max_length+1));
 
-                            int max_length = 0;
-
-                            for (int i = 0; i < table_outputs.Length; i++)
-                            {
-                                if (table_outputs[i].Length > max_length)
-                                {
-                                    max_length = table_outputs[i].Length;
-                                }
-                            }
-
-                            Console.WriteLine(new string('-', max_length+1));
-
-                            for (int i = 0; i < table_outputs.Length; i++)
-                            {
-                                if (table_outputs[i].Length < max_length)
-                                {
-                                    int space_diff = max_length - table_outputs[i].Length;
-                                    for (int j = 0; j < space_diff; j++)
+                                    for (int i = 0; i < table_outputs.Length; i++)
                                     {
-                                        table_outputs[i] += " ";
-                                    }
-                                }
-                                table_outputs[i] += "|";
-                                Console.WriteLine(table_outputs[i]);
-                            }
+                                        if (table_outputs[i].Length < max_length)
+                                        {
+                                            // Basically, adds spaces to match the one with max_length
+                                            int space_diff = max_length - table_outputs[i].Length;
+                                            for (int j = 0; j < space_diff; j++)
+                                            {
+                                                table_outputs[i] += " ";
+                                            }
+                                        }
 
-                            Console.WriteLine(new string('-', max_length+1));
+                                        table_outputs[i] += "|";
+
+                                        Console.WriteLine(table_outputs[i]);
+                                    }
+
+                                    // Make a bottom line string
+                                    Console.WriteLine(new string('-', max_length+1));
+
+                                    /* Display in a nice table - end */
+                            }
                        }
                    }
+                   /* --- End of Summary function --- */
                    else
                    {
+                        /* --- Start of Help function --- */
                         if (args[0] == "help")
                         {
                             Console.WriteLine("List of commands available: ");
@@ -154,12 +194,19 @@ namespace Fenwick_Technical_Task
                             Console.WriteLine("<> - placeholder");
                             Console.WriteLine("[] - optional");
                         }
+                        /* --- End of Help function --- */
+                        else
+                        {
+                            // The first argument is not recognized
+                            Console.WriteLine("Invalid command: Command not recognized.");
+                        }
                    }
                }
             }  
               
             else
             {
+                // No arguments provided
                 Console.WriteLine("Invalid command: No command line arguments found.");
             }
         }
